e0fb5a97ec9abb4cc261aac4178c6e712df5136a
Malbolge <38575453+Rupeshcdy@users.noreply.github.com>
2024-03-07 20:44:28 +0545

    Tailwind css , Shadcn theme stater templete

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { ModeToggle } from "@/components/ModeToggle"
+import { Button } from "@/components/ui/button";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <ModeToggle/>
+      <h1>Hello</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
